c1848daae1976b9e23e23d379ff6ab5e4468f8d3
Jonathan Helmus <jjhelmus@gmail.com>
2015-05-05 12:36:03 -0500

    Update pynio recipe to support shapefiles

--- recipes/pynio/build.sh
+++ recipes/pynio/build.sh
@@ -7,12 +7,12 @@ export NETCDF4_PREFIX=$PREFIX
 export HAS_NETCDF4=1
 export HAS_HDF5=1
 
-export HAS_HDFEOS=0
 export HAS_HDF4=1
 
-export HAS_GDAL=0
+export HAS_GDAL=1
+
+export HAS_HDFEOS=0
 export HAS_HDFEOS5=0
 export HAS_GRIB2=0
 
-export C_INCLUDE_PATH=$PREFIX/include/hdf
 $PYTHON setup.py install
