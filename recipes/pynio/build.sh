a5106d76df3bfcaf4f30360af1aa4a455957c7e8
Jonathan Helmus <jjhelmus@gmail.com>
2015-05-05 14:59:17 -0500

    Updated pynio recipe to add support for GRIB2

--- recipes/pynio/build.sh
+++ recipes/pynio/build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-cp -r $RECIPE_DIR/.. .
 export HAS_NETCDF4=1
 export NETCDF4_PREFIX=$PREFIX
-
 export HAS_NETCDF4=1
 export HAS_HDF5=1
-
 export HAS_HDF4=1
-
 export HAS_GDAL=1
-
 export HAS_HDFEOS=1
-
 export HAS_HDFEOS5=1
-
-export HAS_GRIB2=0
+export HAS_GRIB2=1
 
 $PYTHON setup.py install
