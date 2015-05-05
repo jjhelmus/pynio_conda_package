aba798e3978dc5f8b33c92329235ebc316a8b794
Jonathan Helmus <jjhelmus@gmail.com>
2015-05-05 14:06:21 -0500

    Add hdfeos2 recipe

---
+++ recipes/hdfeos2/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CXXFLAGS="${CFLAGS}"
+
+./configure --prefix=$PREFIX \
+    --with-hdf4=$PREFIX \
+    --enable-install-include 
+make
+make install
